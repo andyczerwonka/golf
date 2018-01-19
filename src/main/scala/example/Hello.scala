45003d77f5b64a645364ac4806e2ebd11294c19d
Andy Czerwonka <andy.czerwonka@gmail.com>
2018-01-19 16:34:34 -0700

    get the list of starting indexes

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,7 +1,10 @@
 package example
 
 object Hello extends Greeting with App {
-  println(greeting)
+
+  val xs = List(1, 3, 5, -9, 11, 12, 13, 15, 78484348, 11, 12 ,13, 0, -1, 343, 1222, 0)
+  val is = 0 :: xs.zipWithIndex.collect { case (v, i) if i > 0 && v <= xs(i-1) => i }
+  println(is)
 }
 
 trait Greeting {
