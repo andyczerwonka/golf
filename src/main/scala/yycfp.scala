a18846e1f4c4a5406302913319c176855065287a
Andy Czerwonka <andy.czerwonka@gmail.com>
2018-02-14 11:43:07 -0700

    abstract our the Int type into something more general... use the Ordering typeclass

--- src/main/scala/yycfp.scala
+++ src/main/scala/yycfp.scala
@@ -2,11 +2,11 @@
 
 package object yycfp {
 
-  def longestContiguousIncreasingRange(xs: Seq[Int]): Option[(Int, Int)] = {
-    if (xs != Nil) {
+  def longestContiguousIncreasingRange[T: Ordering](xs: Seq[T]): Option[(Int, Int)] = {
+    if (xs.nonEmpty) {
       // we'll use this locally scoped partial to return the index when the current value is <=previous
-      val decreasingChange: PartialFunction[(Int, Int), Int] = {
-        case (x, i) if i > 0 && x <= xs(i - 1) => i
+      val decreasingChange: PartialFunction[(T, Int), Int] = {
+        case (x, i) if i > 0 && implicitly[Ordering[T]].lteq(x, xs(i - 1)) => i
       }
 
       // detect all the changes based on the decreasingChange function above
