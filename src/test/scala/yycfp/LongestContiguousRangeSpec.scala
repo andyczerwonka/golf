bc47803d3293b498d4a0d49fd1d2586cf7a2419d
Andy Czerwonka <andy.czerwonka@gmail.com>
2018-02-15 14:54:17 -0700

    use more idiomatic ScalaTest matchers

--- src/test/scala/yycfp/LongestContiguousRangeSpec.scala
+++ src/test/scala/yycfp/LongestContiguousRangeSpec.scala
@@ -6,23 +6,23 @@ class LongestContiguousRangeSpec extends FlatSpec with Matchers {
 
   "The longestContiguousIncreasingRange function" should "evaluate to (3, 8) in Lion's example" in {
     val xs = Vector(1, 3, 5, -9, 11, 12, 13, 15, 78484348, 11, 12 ,13, 0, -1, 343, 1222, 0)
-    longestContiguousIncreasingRange(xs) shouldEqual Some(3, 8)
+    longestContiguousIncreasingRange(xs) should contain (3, 8)
   }
 
-  it should "work for other types" in {
-    longestContiguousIncreasingRange(Vector('a', 'c', 'b', 'd')) shouldEqual Some(0, 1)
+  it should "work also for other non-numeric types" in {
+    longestContiguousIncreasingRange(Vector('a', 'c', 'b', 'd')) should contain (0, 1)
   }
 
   it should "return (0, 0) if the input sequence decreases" in {
-    longestContiguousIncreasingRange(Vector(5, 4, 3, 2, 1)) shouldEqual Some(0, 0)
+    longestContiguousIncreasingRange(Vector(5, 4, 3, 2, 1)) should contain (0, 0)
   }
 
   it should "return (0, 0) if the input sequence contains one element" in {
-    longestContiguousIncreasingRange(Vector(1)) shouldEqual Some(0, 0)
+    longestContiguousIncreasingRange(Vector(1)) should contain (0, 0)
   }
 
   it should "return (0, 1) if the input sequence contains two increasing elements" in {
-    longestContiguousIncreasingRange(Vector(1, 2)) shouldEqual Some(0, 1)
+    longestContiguousIncreasingRange(Vector(1, 2)) should contain (0, 1)
   }
 
   it should "return None when the input sequence is empty" in {
